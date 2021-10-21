bb0e28ac71d1d12ccc54cf5f543ca6a495b033a6
Dante <dante_padley@yahoo.com>
2021-10-20 19:23:49 -0700

    Hooked up the API, figured out backspacing and extra symbols

--- script.js
+++ script.js
@@ -1,67 +1,119 @@
-$(document).ready(function(){
+const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
+
+$(document).ready(function () {
 	let wordCount = 0;
 	let characterCount = 0;
 	let errorCount = 0;
-  let wpm = 0;
-	let generatedQuote = `There once was a story of a man named Wilbur, who was a very good cat, a noble one who was better than all of the rest. One day, Wilbur decided to leave home a bit early to go to work, as he had an important meeting in the morning. You see, Wilbur was a full-time banker at a nearby AME, he had a wife and 2 kids, but Wilbur was unsatisfied with life's direction. He was tired of the same old routine and yearned for some true excitement in his life. This morning, however, this morning was very different, and it was the morning Wilbur would make a true change in his life.`;
+	let wpm = 0;
+	let generatedQuote = ``;
 	let wcEl = $("#word-count");
 	let ccEl = $("#character-count");
 	let ecEl = $("#error-count");
 	let quoteEl = $("#generated-quote");
 	let wordsperminEl = $("#wordspermin");
-	let quoteChars = generatedQuote.split("");
+	let quoteChars = '';
 	let userChars = [];
 	let currentIndex = 0;
 	let charTyped = null;
 	let block = false;
 
-	// Generate quote element
-	for (let i = 0; i < generatedQuote.length; i++) {
-		quoteEl.append("<span>" + generatedQuote[i] + "</span>");
+	// this function uses the API to fetch the actual quote
+	function getRandomQuote() {
+		return fetch(RANDOM_QUOTE_API_URL)
+			.then((response) => response.json())
+			.then((data) => data.content);
 	}
 
-	// Input handling
-	$('body').bind('keypress', function(e) {
-		charTyped = String.fromCharCode(e.keyCode);
-		if (charTyped !== quoteChars[currentIndex]) {
-			if (block !== true) {
-				$("span").eq(currentIndex).css("background-color", "#e32957");
-				errorCount++;
-				ecEl.text("Errors: " + errorCount.toString());
-				currentIndex++;
-				block = true;
-			}
-		}
-		else {
-			$("span").eq(currentIndex).css("background-color", "#8cff78");
-			characterCount++;
-			ccEl.text("Characters Typed: " + characterCount.toString());
-			if (charTyped === ' ') {
-				wordCount++;
-				wcEl.text("Words Typed: " + wordCount.toString());
-				block = false;
-			}
-			currentIndex++;
-		}
+	// this function handles the quote and puts the quote into the quoteDisplay element to display it to the user.
+	async function renderNewQuote() {
+		const quote = await getRandomQuote();
+		quoteEl.empty();
+		currentIndex = 0;
+		// going through a loop that gets each individual character in the string, creating a span for it, and then setting the text to that span to the individual character. This makes it so we can apply individual colors to each one of our letters as needed.
+		quote.split("").forEach((character) => {
+			const characterSpan = document.createElement("span");
+			characterSpan.innerText = character;
+			quoteEl.append(characterSpan);
+		});
+		quoteChars = quote.split("");
+		//quoteEl.value = null;
+	}
+	renderNewQuote()
 
-		if (currentIndex === quoteChars.length - 1) {
+
+
+
+	// Input handling
+	$('body').bind('keypress', function (e) {
+		charTyped = String.fromCharCode(e.keyCode);
+		if (/^[a-zA-Z0-9]+$/.test(charTyped) || /[~`!#$%\^&*+= \-\[\]\\'';,/{}|\\":<>\?]+$/.test(charTyped) || charTyped == '.' || charTyped == "'") {
+			if (charTyped !== quoteChars[currentIndex]) {
+				//if (block !== true) {
+					$("span").eq(currentIndex).css("background-color", "#e32957");
+					$("span").eq(currentIndex).addClass("wrong")
+					errorCount++;
+					ecEl.text("Errors: " + errorCount.toString());
+					currentIndex++;
+					//block = true;
+				//}
+			}
+			else {
+				$("span").eq(currentIndex).css("background-color", "#8cff78");
+				characterCount++;
+				ccEl.text("Characters Typed: " + characterCount.toString());
+				if (charTyped === ' ') {
+					wordCount++;
+					wcEl.text("Words Typed: " + wordCount.toString());
+					//block = false;
+				}
+				currentIndex++;
+			}
+		}
+		
+
+		if (currentIndex === quoteChars.length) {
 			wordspermin();
+			renderNewQuote();
 			return;
 		}
 	});
+	//Keybind specifically for backspace. Requires binding to "keydown" instead of "keypress"
+	$('body').bind('keydown', function (e) {
+		if (currentIndex > 0) {
+			charTyped = e.keyCode;
+			if (charTyped == 8) {
+				/// We want to track the wordcount better, probably by counting every 5 characters
+				/// Then figure out how to lower wordcount when backspacing
+				/*if ($("span").eq(currentIndex).textContent == '\u00A0'){
+					wordCount--;
+					wcEl.text("Words Typed: " + wordCount.toString());
+				}*/
+				currentIndex--;
+				characterCount--;
+				ccEl.text("Characters Typed: " + characterCount.toString());
+				
+				if ($("span").eq(currentIndex).hasClass("wrong")) {
+					errorCount--;
+					ecEl.text("Errors: " + errorCount.toString());
+				}
+				$("span").eq(currentIndex).css("background-color", "#00000000");
+			}
+		}
+	});
+	
 
-    let timerlimit = 1;
-  	function wordspermin(){
-      wpm = Math.round(((characterCount / 5) / timerlimit));
-      wordsperminEl.text("WPM: " + wpm.toString());
-    }
+	let timerlimit = 1;
+	function wordspermin() {
+		wpm = Math.round(((characterCount / 5) / timerlimit));
+		wordsperminEl.text("WPM: " + wpm.toString());
+	}
 
 	// Timer
 	let secondsRemaining = 60;
 	let timerEl = $("#timer");
-	
-	let interval = setInterval(function() { 
-    timerEl.text("Seconds Remaining: " + secondsRemaining.toString());
+
+	let interval = setInterval(function () {
+		timerEl.text("Seconds Remaining: " + secondsRemaining.toString());
 		if (secondsRemaining > 0) {
 			secondsRemaining--;
 		}
@@ -71,4 +123,6 @@ $(document).ready(function(){
 			return;
 		}
 	}, 1000);
+
+	
 });
