ae4ec141327df56908dd91c0f4c89c943d1cf118
emily le <emilytwo19@comcast.net>
2021-11-11 16:44:31 -0800

    let main-title fade out

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
 		if (secondsRemaining > 0) TextCounter();
 
 		//starts timer
-		if (block == false) {Start_timer(); block = true; myobj.remove();}
+		if (block == false) {Start_timer(); block = true; $(myobj).fadeOut(2000);}
 
 		// if (/^[a-zA-Z0-9]+$/.test(charTyped) || /[~`!#$%\^&*+= \-\[\]\\'';,/{}|\\":<>\?]+$/.test(charTyped) || charTyped == '.' || charTyped == "'") {
 
